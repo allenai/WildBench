90ad32ee18432104991771c90752e111d22947bb
da03 <dengyuntian@gmail.com>
2024-03-07 02:37:03 -0800

    .

--- scripts/cohere-command.sh
+++ scripts/cohere-command.sh
@@ -4,8 +4,8 @@ output_dir="result_dirs/wild_bench/"
 TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
 
 # shard_size should be 1024 // n_shards
-n_shards=16
-shard_size=64
+n_shards=8
+shard_size=128
 start_gpu=0
 shards_dir="${output_dir}/tmp_${model_pretty_name}"
 for ((start = 0, end = (($shard_size)), gpu = $start_gpu; gpu < $n_shards+$start_gpu; start += $shard_size, end += $shard_size, gpu++)); do
