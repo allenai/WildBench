4350c6fce7864f22bcdf1493fa57173f8e8bf9b2
yuchenlin <yuchen.lin@usc.edu>
2024-07-03 07:43:41 +0000

    update data prefix in script

--- evaluation/run_score_eval_batch.sh
+++ evaluation/run_score_eval_batch.sh
@@ -1,6 +1,6 @@
 # Starting 0612, we will use "gpt-4o-2024-05-13" for evaluation
 MODEL=$1 # your model name
 bash evaluation/run_eval_v2_batch.score.sh $MODEL  
-python src/openai_batch_eval/submit_batch.py eval_results/v2.0522/score.v2/eval=gpt-4o-2024-05-13/$MODEL.batch-submit.jsonl
+python src/openai_batch_eval/submit_batch.py eval_results/v2.0625/score.v2/eval=gpt-4o-2024-05-13/$MODEL.batch-submit.jsonl
 
 # python src/openai_batch_eval/check_batch_status_with_model_name.py $MODEL
